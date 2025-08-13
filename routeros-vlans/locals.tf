03c41d3225395e4e23971d0aeff188002094a929
Pieter-Visscher <mail@pvisscher.nl>
2025-08-13 20:57:48 +0200

    change state index, change network variable

--- routeros-vlans/locals.tf
+++ routeros-vlans/locals.tf
@@ -6,8 +6,8 @@ locals {
 locals {
   # Helper to calculate network and address or each VLAN
   vlan_map = {
-    for k, v in var.vlans : k => merge(v, {
-      network = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.0"
+    for k, v in var.vlans : v.name => merge(v, {
+      address = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.0"
       gateway = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.1/24"
     })
   }
