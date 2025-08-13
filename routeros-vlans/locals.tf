056fd29b740a4ef80061beadf4d214a136d25da7
Pieter-Visscher <mail@pvisscher.nl>
2025-08-13 18:10:57 +0200

    syntax fix

--- routeros-vlans/locals.tf
+++ routeros-vlans/locals.tf
@@ -1,9 +1,7 @@
 locals {
   # Convert list to map keyed by VLAN name
   vlan_map = { for vlan in var.vlans : vlan.name => vlan }
-}
-
-locals {
-  # Convert list to map keyed by bridge name
+  # convert list to map keyed by bridge name
   bridge_map = { for bridge in var.bridges : bridge.name => bridge }
 }
+
