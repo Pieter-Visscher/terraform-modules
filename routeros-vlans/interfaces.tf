0323c1e96471754c1fef61d4ef2fbda714a71484
Pieter-Visscher <mail@pvisscher.nl>
2025-08-14 20:42:32 +0200

    add bridge check

--- routeros-vlans/interfaces.tf
+++ routeros-vlans/interfaces.tf
@@ -1,6 +1,8 @@
 resource "routeros_interface_bridge_vlan" "bridge_vlan" {
   for_each = local.vlan_map
 
+  count = contains(keys(local.bridge_map, each.value.interface) ? 1 : 0
+
   bridge  = each.value.interface
   vlan_ids = [each.value.id]
   tagged = each.value.tagged_ports
