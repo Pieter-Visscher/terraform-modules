f0c2719f526277289f0b5da9e824e510e0340e42
Pieter-Visscher <mail@pvisscher.nl>
2025-08-14 20:50:04 +0200

    replace count with ifstatement, add concat to tag bridge by default

--- routeros-vlans/interfaces.tf
+++ routeros-vlans/interfaces.tf
@@ -1,10 +1,12 @@
 resource "routeros_interface_bridge_vlan" "bridge_vlan" {
-  for_each = local.vlan_map
-
-  count = contains(keys(local.bridge_map), each.value.interface) ? 1 : 0
+  for_each = {
+    for vlan_name, vlan_data in local.vlan_map :
+    vlan_name => vlan_data
+    if contains(keys(local.bridge_map), vlan_data.interface)
+  }
 
   bridge  = each.value.interface
   vlan_ids = [each.value.id]
-  tagged = each.value.tagged_ports
+  tagged = concat(each.value.tagged_ports, each.value.interface)
   untagged = each.value.untagged_ports
 }
