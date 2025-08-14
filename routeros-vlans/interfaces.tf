15bc75fc8303f1f0d1ef766579ab35a2bab5e287
Pieter-Visscher <mail@pvisscher.nl>
2025-08-14 20:51:35 +0200

    add distinct, add brackets syntax fix

--- routeros-vlans/interfaces.tf
+++ routeros-vlans/interfaces.tf
@@ -7,6 +7,6 @@ resource "routeros_interface_bridge_vlan" "bridge_vlan" {
 
   bridge  = each.value.interface
   vlan_ids = [each.value.id]
-  tagged = concat(each.value.tagged_ports, each.value.interface)
+  tagged = distinct(concat(each.value.tagged_ports, [each.value.interface]))
   untagged = each.value.untagged_ports
 }
