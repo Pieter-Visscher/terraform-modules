288ce3410efaad5b89d72ebbc2419743202d6ada
Pieter-Visscher <mail@pvisscher.nl>
2025-08-16 20:51:21 +0200

    add quotes to strings

--- routeros-interfaces/wan.tf
+++ routeros-interfaces/wan.tf
@@ -1,7 +1,7 @@
 resource "routeros_ip_dhcp_client" "wan" {
   count = var.wan.dhcp == true ? 1 : 0
 
-  add_default_route = yes
+  add_default_route = "yes"
   interface         = var.wan.interface
-  comment           = WAN
+  comment           = "WAN"
 }
