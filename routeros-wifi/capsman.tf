5d849ac165e5c83bdb3851eadd6441ac3d322ae4
Pieter-Visscher <mail@pvisscher.nl>
2025-08-20 22:27:22 +0200

    add provisioning rule

--- routeros-wifi/capsman.tf
+++ routeros-wifi/capsman.tf
@@ -3,3 +3,9 @@ resource "routeros_wifi_capsman" "capsman" {
   upgrade_policy = "suggest-same-version"
   interfaces     = var.CAPsMAN_interfaces
 }
+
+resource "routeros_wifi_provisioning" "provisioning" {
+  action    = "create-dynamic-enabled"
+  master_configuration = var.wifi_config[0].name
+  slave_configurations = [for config in slice(var.wifi_config, 1, length(var.wifi_config)) : config.name]
+  }
