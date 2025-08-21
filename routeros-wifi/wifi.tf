7bb724206fef86ee8524649fc5451b6b0eb22a5f
Pieter-Visscher <mail@pvisscher.nl>
2025-08-21 21:24:21 +0200

    remove debugging

--- routeros-wifi/wifi.tf
+++ routeros-wifi/wifi.tf
@@ -3,10 +3,10 @@ resource "routeros_wifi_channel" "channels" {
 
   name              = each.value.name
   band              = each.value.band
-  #width             = each.value.channel_width
+  width             = each.value.channel_width
   skip_dfs_channels = each.value.skip_dfs
   reselect_interval = each.value.reselect_interval
-  #frequency         = each.value.frequency
+  frequency         = each.value.frequency
 }
 
 resource "routeros_wifi_datapath" "datapaths" {
