10d84a0f779a0a099b6aaaaa3d23d657c9a471f1
Pieter-Visscher <mail@pvisscher.nl>
2025-08-30 15:09:36 +0200

    add updated locals for dhcp_network

--- routeros-dhcp/locals.tf
+++ routeros-dhcp/locals.tf
@@ -3,6 +3,8 @@ locals {
   # Helper to calculate network and address or each VLAN
   vlan_map = {
     for k, v in var.vlans : v.name => merge(v, {
+      dhcp_gateway = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.1"
+      dhcp_network = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.0/24"
       pool_start = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.${var.dhcp_range[0]}"
       pool_end = "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${v.id}.${var.dhcp_range[1]}"
     })
