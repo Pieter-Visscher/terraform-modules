d7cd6124df10da95957b7749eb6bc4ea34b9164a
Pieter-Visscher <mail@pvisscher.nl>
2025-08-30 15:09:22 +0200

    add dhcp server network

--- routeros-dhcp/dhcp.tf
+++ routeros-dhcp/dhcp.tf
@@ -4,14 +4,27 @@ resource "routeros_ip_dhcp_server" "dhcp_server" {
     if v.dhcp
   }
 
-  interface     =  each.value.name
-  address_pool  = "${each.value.name}-pool"
-  name          = "${each.value.name}-dhcp-server"
-  comment       = each.value.comment
+  interface           = each.value.name
+  address_pool        = "${each.value.name}-pool"
+  name                = "${each.value.name}-dhcp-server"
+  conflict_detection  = true
+  comment             = each.value.comment
 
-  depends_on = [resource.routeros_ip_pool.pools]
+  depends_on = [resource.routeros_ip_pool.pools, routeros_ip_dhcp_server_network.dhcp_server_network]
 }
 
+resource "routeros_ip_dhcp_server_network" "dhcp_server_network" {d
+  for_each =  {
+    for v in local.vlan_map : v.name => v
+    if v.dhcp
+  }
+
+  address     = each.value.dhcp_network
+  gateway     = each.value.dhcp_gateway
+  dns_server  = each.value.dhcp_gateway
+  netmask     = "255.255.255.0"
+  comment     = each.value.comment
+}
 resource "routeros_ip_pool" "pools" {
   for_each =  {
     for v in local.vlan_map : v.name => v
