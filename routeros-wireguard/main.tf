4b98bf4fbb720c85a39c5243550a8b9d574bc873
Pieter-Visscher <mail@pvisscher.nl>
2025-09-17 20:38:07 +0200

    add omit handling

--- routeros-wireguard/main.tf
+++ routeros-wireguard/main.tf
@@ -16,7 +16,7 @@ resource "routeros_interface_wireguard_peer" "wireguard-peer" {
 
   interface       = routeros_interface_wireguard.wireguard-interface.name
   public_key      = each.value.public_key
-  allowed_address = concat(
+  allowed_address = each.value.subnets[0] == "omit" ? "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${var.vpn_interface_subnet}.${each.value.id}/32" : concat(
   each.value.subnets,
   [
     "${split(".", cidrhost(var.default_cidr, 0))[0]}.${split(".", cidrhost(var.default_cidr, 0))[1]}.${var.vpn_interface_subnet}.${each.value.id}/32"
