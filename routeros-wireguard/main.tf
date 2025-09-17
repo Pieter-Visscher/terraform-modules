521e2e229ba4fa7048e49368d311fafe4241589c
Pieter-Visscher <mail@pvisscher.nl>
2025-09-17 16:25:28 +0200

    add interface address

--- routeros-wireguard/main.tf
+++ routeros-wireguard/main.tf
@@ -2,3 +2,8 @@ resource "routeros_interface_wireguard" "wireguard-interface" {
   name  = "wireguard"
   listen_port = 51820
 }
+
+resource "routeros_ip_address" {
+  address = var.vpn_interface_address
+  interface = "wireguard"
+}
---
+++ routeros-wireguard/variables.tf
@@ -0,0 +1,4 @@
+variable "vpn_interface_address" {
+  type = string
+  description = "address bound to the wireguard interface in CIDR notation"
+}
