49d76aff01100735d24e247658e051662f98fa04
Pieter-Visscher <mail@pvisscher.nl>
2025-09-20 20:29:23 +0200

    syntax fix

--- routeros-wifi/capsman.tf
+++ routeros-wifi/capsman.tf
@@ -4,13 +4,13 @@ resource "routeros_wifi_capsman" "capsman" {
   interfaces     = var.CAPsMAN_interfaces
 }
 
-resource "routeros_wifi_provisioning" "24-provisioning" {
+resource "routeros_wifi_provisioning" "24_provisioning" {
   action    = "create-dynamic-enabled"
   master_configuration = var.wifi_config-24ghz[0].name
   slave_configurations = [for config in slice(var.wifi_config, 1, length(var.wifi_config)) : config.name]
   }
 
-resource "routeros_wifi_provisioning" "5-provisioning" {
+resource "routeros_wifi_provisioning" "5_provisioning" {
   action    = "create-dynamic-enabled"
   master_configuration = var.wifi_config-5ghz[0].name
   slave_configurations = [for config in slice(var.wifi_config, 1, length(var.wifi_config)) : config.name]
