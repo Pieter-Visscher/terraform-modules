6217a12882d833e3393379023a1f2a46c5264646
Pieter-Visscher <mail@pvisscher.nl>
2025-09-20 21:53:19 +0200

    fix logic

--- routeros-wifi/capsman.tf
+++ routeros-wifi/capsman.tf
@@ -6,14 +6,14 @@ resource "routeros_wifi_capsman" "capsman" {
 
 resource "routeros_wifi_provisioning" "provisioning_24ghz" {
   action    = "create-dynamic-enabled"
-  supported_bands      = [for config in var.wifi_config-24ghz : config.band]
+  supported_bands = distinct([for ch in var.wifi_channel : ch.band if contains([for cfg in var.wifi_config-24ghz : cfg.channel], ch.name)])
   master_configuration = var.wifi_config-24ghz[0].name
   slave_configurations = [for config in slice(var.wifi_config-24ghz, 1, length(var.wifi_config-24ghz)) : config.name]
   }
 
 resource "routeros_wifi_provisioning" "provisioning_5ghz" {
   action    = "create-dynamic-enabled"
-  supported_bands      = [for config in var.wifi_config-5ghz: config.band]
+  supported_bands = distinct([for ch in var.wifi_channel : ch.band if contains([for cfg in var.wifi_config-5ghz : cfg.channel], ch.name)])
   master_configuration = var.wifi_config-5ghz[0].name
   slave_configurations = [for config in slice(var.wifi_config-5ghz, 1, length(var.wifi_config-5ghz)) : config.name]
   }
