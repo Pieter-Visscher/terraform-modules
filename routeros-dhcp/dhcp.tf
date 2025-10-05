c5e1565937e55d895d252031011b61e1340e1069
Pieter-Visscher <mail@pvisscher.nl>
2025-10-05 14:02:55 +0200

    add dependency

--- routeros-dhcp/dhcp.tf
+++ routeros-dhcp/dhcp.tf
@@ -11,6 +11,8 @@ resource "routeros_ip_dhcp_server_option_set" "dhcp_option_sets" {
 
   name      = each.value.name
   options   = each.value.options
+
+  depends_on  = [resource.ip_dhcp_server_opiton.dhcp_options]
 }
 
 resource "routeros_ip_dhcp_server" "dhcp_server" {
