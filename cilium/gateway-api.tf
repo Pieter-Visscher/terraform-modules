d765a8bec7a6a458b5bc4e233a95e4f2669ae9af
Pieter-Visscher <mail@pvisscher.nl>
2025-11-29 14:23:54 +0100

    fix incorrect code

--- cilium/gateway-api.tf
+++ cilium/gateway-api.tf
@@ -1,7 +1,14 @@
+resource "kubernetes_manifest" "gateway_api_install" {
+  manifest = yamldecode(data.http.gateway_api_manifest.response_body)
+}
 data "http" "gateway_api_manifest" {
-  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml"
+  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/${var.gateway_api_version}/standard-install.yaml"
+  response_body_allow_list = ["text/plain", "application/yaml", "text/yaml"]
+  
+  timeout = 30s
 }
 
 resource "kubernetes_manifest" "gateway_api_install" {
   manifest = yamldecode(data.http.gateway_api_manifest.response_body)
+  server_side_apply = true
 }
