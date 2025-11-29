7acc4b4611b61f35c9648c2f95962640b74cdcb7
Pieter-Visscher <mail@pvisscher.nl>
2025-11-29 14:19:39 +0100

    fix incorrect code

--- cilium/gateway-api.tf
+++ cilium/gateway-api.tf
@@ -1,12 +1,5 @@
 data "http" "gateway_api_manifest" {
   url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml"
-  
-  lifecycle {
-    precondition {
-      condition     = length(data.http.gateway_api_manifest.response_body) > 0
-      error_message = "Failed to fetch the Gateway API manifest from the URL."
-    }
-  }
 }
 
 resource "kubernetes_manifest" "gateway_api_install" {
