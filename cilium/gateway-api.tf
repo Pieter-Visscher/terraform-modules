ec1ca8771aa218e9ee1c7261e0d7c9d36183e646
Pieter-Visscher <mail@pvisscher.nl>
2025-11-29 14:47:08 +0100

    stuff

--- cilium/gateway-api.tf
+++ cilium/gateway-api.tf
@@ -5,18 +5,32 @@ data "http" "gateway_api_manifest" {
 locals {
   raw_docs = split("\n---", data.http.gateway_api_manifest.response_body)
 
-  gateway_docs = [
-    for doc in local.raw_docs :
-    trimspace(doc)
-    if trimspace(doc) != ""
+  decoded_docs = [
+    yamldecode(doc)
+    for doc in local.raw_docs
+    if(trimspace(doc) != "")
+    if !startswith(trimspace(doc), "#")
+    if can(yamldecode(doc))
+    if try(yamldecode(doc).kind, "") != ""
+  ]
+
+  cleaned_docs = [
+    merge(
+      doc,
+      doc.kind == "CustomResourceDefinition" ?
+        { status = null } :
+        {}
+    )
+    for doc in local.decoded_docs
   ]
 }
 
 resource "kubernetes_manifest" "gateway_api" {
   for_each = {
-    for idx, doc in local.gateway_docs :
+    for idx, doc in local.cleaned_docs :
     idx => doc
   }
 
-  manifest = yamldecode(each.value)
+  manifest = each.value
 }
+
