d7eb503fa8d913ca1377b6faeacd3bdb9fdc9afc
Pieter-Visscher <mail@pvisscher.nl>
2025-11-29 14:44:55 +0100

    fix incorrect code

--- cilium/gateway-api.tf
+++ cilium/gateway-api.tf
@@ -2,20 +2,20 @@ data "http" "gateway_api_manifest" {
   url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/${var.gateway_api_version}/standard-install.yaml"
 }
 
-data "kubectl_file_documents" "gateway_api_manifest" {
-  content = data.http.gateway_api_manifest.response_body
-  lifecycle {
-    precondition {
-      condition     = data.http.gateway_api_manifest.status_code == 200
-      error_message = "Failed to download Gateway API manifest"
-    }
-  }
+locals {
+  raw_docs = split("\n---", data.http.gateway_api_manifest.response_body)
+
+  gateway_docs = [
+    for doc in local.raw_docs :
+    trimspace(doc)
+    if trimspace(doc) != ""
+  ]
 }
 
 resource "kubernetes_manifest" "gateway_api" {
   for_each = {
-    for i, doc in data.kubectl_file_documents.gateway_api_manifest.documents :
-    i => doc
+    for idx, doc in local.gateway_docs :
+    idx => doc
   }
 
   manifest = yamldecode(each.value)
