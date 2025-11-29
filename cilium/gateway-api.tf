34513054e9978dd81a0cbbb41caa93e399824437
Pieter-Visscher <mail@pvisscher.nl>
2025-11-29 14:18:42 +0100

    fix incorrect code

--- cilium/gateway-api.tf
+++ cilium/gateway-api.tf
@@ -1,8 +1,9 @@
 data "http" "gateway_api_manifest" {
-  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/${var.gateway_api_version}/standard-install.yaml"
+  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml"
+  
   lifecycle {
     precondition {
-      condition     = length(self.response_body) > 0
+      condition     = length(data.http.gateway_api_manifest.response_body) > 0
       error_message = "Failed to fetch the Gateway API manifest from the URL."
     }
   }
