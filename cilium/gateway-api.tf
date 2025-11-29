7bead8c0647d827878472a4848d47ef00e4aeda6
Pieter-Visscher <mail@pvisscher.nl>
2025-11-29 14:42:54 +0100

    fix incorrect code

--- cilium/gateway-api.tf
+++ cilium/gateway-api.tf
@@ -1,18 +1,22 @@
 data "http" "gateway_api_manifest" {
-  url = "https://github.com/cert-manager/cert-manager/releases/download/v${var.cert_manager_version}/cert-manager.crds.yaml"
+  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/${var.gateway_api_version}/standard-install.yaml"
 }
 
 data "kubectl_file_documents" "gateway_api_manifest" {
   content = data.http.gateway_api_manifest.response_body
   lifecycle {
     precondition {
-      condition     = 200 == data.http.cert_manager_crds.status_code
-      error_message = "Status code invalid"
+      condition     = data.http.gateway_api_manifest.status_code == 200
+      error_message = "Failed to download Gateway API manifest"
     }
   }
 }
 
-resource "kubernetes_manifest" "gateway_api_manifest" {
-  for_each  = data.kubectl_file_documents.gateway_api_manifest
-  yaml_body = each.value
+resource "kubernetes_manifest" "gateway_api" {
+  for_each = {
+    for i, doc in data.kubectl_file_documents.gateway_api_manifest.documents :
+    i => doc
+  }
+
+  manifest = yamldecode(each.value)
 }
