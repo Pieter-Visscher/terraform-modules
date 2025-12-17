38c1c8bc8e45d442dc4fc56bfcf654859b57459d
Pieter-Visscher <mail@pvisscher.nl>
2025-12-17 18:40:05 +0100

    add kubevirt and cdi installations

--- kubevirt/main.tf
+++ kubevirt/main.tf
@@ -17,11 +17,11 @@ resource "kubectl_manifest" "kubevirt_operator" {
   yaml_body = each.value
 }   
 
-data "http" "kubevirt_cdi" {
+data "http" "kubevirt_cdi_operator" {
   url = "https://github.com/kubevirt/containerized-data-importer/releases/download/${var.kubevirt_cdi_version}/cdi-operator.yaml"
 }
 
-data "kubectl_file_documents" "kubevirt_cdi" {
+data "kubectl_file_documents" "kubevirt_cdi_operator" {
   content = data.http.kubevirt_cdi.response_body
   lifecycle {
     precondition {
@@ -31,7 +31,67 @@ data "kubectl_file_documents" "kubevirt_cdi" {
   }
 }
 
-resource "kubectl_manifest" "kubevirt_cdi" {
+resource "kubectl_manifest" "kubevirt_cdi_operator" {
   for_each  = data.kubectl_file_documents.kubevirt_cdi.manifests
   yaml_body = each.value
 }   
+
+resource "kubernetes_manifest" "kubevirt" {
+  manifest = {
+    apiVersion = "kubevirt.io/v1"
+    kind = "KubeVirt"
+    metadata = {
+      name = "kubevirt"
+      namespace = "kubevirt"
+    }
+    spec = {
+      configuration = {
+        developerConfiguration = {
+          featureGates = [
+            "LiveMigration", "NetworkBindingPlugins"
+          ]
+        }
+        smbios = {
+          sku = "TalosCloud"
+          version = "v0.1.0"
+          manufacturer = "Talos Virtualization"
+          product = "talosvm"
+          family = "ccio"
+        }
+      }
+      workloadUpdateStrategy = {
+        workloadUpdateMethods = [
+          "LiveMigrate"
+        ]
+      }
+    }
+  }
+  depends_on = [resource.kubectl_manifest.kubevirt_operator]
+}
+
+resource "kubernetes_manifest" "kubevirt_cdi" {
+  manifest = {
+    apiVersion = "cdi.kubevirt.io/v1beta1"
+    kind = "CDI"
+    metadata = {
+      name = "cdi"
+    }
+    spec = {
+      config = {
+        scratchSpaceStorageClass = "longhorn"
+        podResourceRequirements = {
+          requests = {
+            cpu = "100m"
+            memory = "60M"
+            }
+          limits = {
+            cpu = "750m"
+            memory = "2Gi"
+            }
+          }
+        }
+      }
+    }
+  }
+  depends_on = [resource.kubectl_manifest.kubevirt_cdi_operator]
+}
